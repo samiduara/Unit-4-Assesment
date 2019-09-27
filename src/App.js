09aedb195cb4ae108822991b3de1ec49b175f890
sam <samgathon@hotmail.com>
2019-09-27 16:53:50 -0500

    creating components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import CircleSelector from './components/CircleSelector';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      selectedCircle: 1
+    };
+  }
+
+  handleChangeCircle = (circleNumber) => {
+    this.setState({selectedCircle: circleNumber});
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">UNIT 4 FINAL ASSESSMENT</header>
+        <main>
+        <CircleSelector
+            selectedCircle={this.state.selectedCircle}
+            handleChangeCircle={this.handleChangeCircle}
+          />
+ 
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/CircleSelector.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './CircleSelector.css';
+
+const CircleSelector = (props) => (
+    <div className='CircleSelector'>
+<button></button>
+<button></button>
+<button></button>
+        </div>
+
+);
+
+export default CircleSelector;
